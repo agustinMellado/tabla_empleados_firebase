65a49bee8858c848aa3fee0d2372b4866b225578
Agustin Mellado <aguxmellado@gmail.com>
2022-09-11 18:04:13 -0300

    creacion de servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
 import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,7 +10,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     CrearEmpleadoComponent,
     ListaEmpleadosComponent,
     NavbarComponent
