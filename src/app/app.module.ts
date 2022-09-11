08722ab74866ecae8326a303339b08f3c02d9c5a
Agustin Mellado <aguxmellado@gmail.com>
2022-09-11 17:49:32 -0300

    creacion de navbar y tabla empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
-import { CrearEmpleadoComponent } from './componets/crear-empleado/crear-empleado.component';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
 import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
     CrearEmpleadoComponent,
-    ListaEmpleadosComponent
+    ListaEmpleadosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
