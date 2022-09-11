c1437d7bac3a1dec318fd25c55be5a6ec739b2bd
Agustin Mellado <aguxmellado@gmail.com>
2022-09-11 18:19:53 -0300

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+//configuracion de rutas
+const routes: Routes = [
+  {path: 'lista-empleados', component: ListaEmpleadosComponent},
+  {path: 'crear-empleado', component:CrearEmpleadoComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
