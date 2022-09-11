0d619705d52d91a384fcc79124478ff356ad413c
Agustin Mellado <aguxmellado@gmail.com>
2022-09-11 17:38:25 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { CrearEmpleadoComponent } from './componets/crear-empleado/crear-empleado.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    CrearEmpleadoComponent,
+    ListaEmpleadosComponent
   ],
   imports: [
     BrowserModule,
