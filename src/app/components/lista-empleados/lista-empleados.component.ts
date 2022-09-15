21cdb58a5702c42f2b78b4654cd6495f66907399
Agustin Mellado <aguxmellado@gmail.com>
2022-09-15 17:18:56 -0300

    creacion de metodos para mostrar los empleados registrados en la lista-empleados

--- src/app/components/lista-empleados/lista-empleados.component.ts
+++ src/app/components/lista-empleados/lista-empleados.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -8,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-empleados.component.css'],
 })
 export class ListaEmpleadosComponent implements OnInit {
+  //arreglo para almacenar toda la informacion de los empleados.
+  empleados: any[] = [];
 
-  constructor() {
-    
+  constructor(private _empledoService: EmpleadoService) {}
+
+  ngOnInit(): void {
+    //cuando se inicialice el componente, llamo al metodo
+    this.getEmpleados();
   }
+  //toma los empleados y los mete a la lista.
+  getEmpleados() {
+    this._empledoService.getEmpleados().subscribe((data) => {
+      data.forEach((element: any) => {
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.empleados);
+  });
+}
 
-  ngOnInit(): void {}
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,4 +13,11 @@ export class EmpleadoService {
     //creamos una coleccion llamada empleados, le agregamos empleado y retornamos
     return this.firestore.collection('empleado').add(empleado);
   }
+  //agrega los empleados creados a la lista-empleados
+  getEmpleados():Observable<any> {
+    //retorna la coleccion empleados, con snapshotChanges() muestra los cambios en tiempo real.
+    //ref=>ref.orderBy('fechaCreacion','asc') es para indicar q quiero ordenar y de q manera.
+    return this.firestore.collection('empleado',ref=>ref.orderBy('fechaCreacion','asc')).snapshotChanges();
+  }
+
 }
